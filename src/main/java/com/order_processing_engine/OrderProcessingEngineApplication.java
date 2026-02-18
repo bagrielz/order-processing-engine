c76211acf26ae794ba664e92341fb194e0dd69bb
Gabriel Stênio <gabri.stnio50@hotmail.com>
2026-02-18 10:41:51 -0300

    first commit

---
+++ src/main/java/com/order_processing_engine/OrderProcessingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.order_processing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingEngineApplication.class, args);
+	}
+
+}
