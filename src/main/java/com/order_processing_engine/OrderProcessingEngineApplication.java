d46123d795481e8da329501683486c79ed9d1952
Gabriel Stênio <gabri.stnio50@hotmail.com>
2026-02-18 13:13:03 -0300

    ref: updates the OrderItem class

--- src/main/java/com/order_processing_engine/OrderProcessingEngineApplication.java
+++ src/main/java/com/order_processing_engine/OrderProcessingEngineApplication.java
@@ -1,9 +1,13 @@
 package com.order_processing_engine;
 
 import com.order_processing_engine.domains.OrderItem;
+import com.order_processing_engine.domains.valueobject.Money;
+import com.order_processing_engine.domains.valueobject.Quantity;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class OrderProcessingEngineApplication implements CommandLineRunner {
@@ -14,7 +18,10 @@ public class OrderProcessingEngineApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var item = new OrderItem("Mouse", 2, 100);
-		System.out.println(item.totalValue());
+		var quantity = new Quantity(2);
+		var unitPrice = new Money(new BigDecimal("100"));
+
+		var item = new OrderItem("Mouse", quantity, unitPrice);
+		System.out.println(item.totalValue().getValue());
 	}
 }
--- src/main/java/com/order_processing_engine/domains/OrderItem.java
+++ src/main/java/com/order_processing_engine/domains/OrderItem.java
@@ -1,17 +1,20 @@
 package com.order_processing_engine.domains;
 
+import com.order_processing_engine.domains.valueobject.Money;
+import com.order_processing_engine.domains.valueobject.Quantity;
+
 public class OrderItem {
     private final String productName;
-    private final int quantity;
-    private final double unitPrice;
+    private final Quantity quantity;
+    private final Money unitPrice;
 
-    public OrderItem(String productName, int quantity, double unitPrice) {
+    public OrderItem(String productName, Quantity quantity, Money unitPrice) {
         this.productName = productName;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
     }
 
-    public double totalValue() {
-        return this.quantity * this.unitPrice;
+    public Money totalValue() {
+        return unitPrice.multiply(quantity.getValue());
     }
 }
