24ddb2a356f27beb1f196ac2f5d5e318ec239486
Gabriel Stênio <gabri.stnio50@hotmail.com>
2026-02-27 08:53:08 -0300

    feat: add the method that defines the discount percentage

--- src/main/java/com/order_processing_engine/domains/enums/CustomerType.java
+++ src/main/java/com/order_processing_engine/domains/enums/CustomerType.java
@@ -1,7 +1,26 @@
 package com.order_processing_engine.domains.enums;
 
+import java.math.BigDecimal;
+
 public enum CustomerType {
-    NORMAL,
-    PREMIUM,
-    VIP
+    NORMAL {
+        @Override
+        public BigDecimal discountPercentage() {
+            return BigDecimal.ZERO;
+        }
+    },
+    PREMIUM {
+        @Override
+        public BigDecimal discountPercentage() {
+            return new BigDecimal("0.10");
+        }
+    },
+    VIP {
+        @Override
+        public BigDecimal discountPercentage() {
+            return new BigDecimal("0.15");
+        }
+    };
+
+    public abstract BigDecimal discountPercentage();
 }
